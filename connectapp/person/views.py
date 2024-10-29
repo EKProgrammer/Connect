eacf4896087f6fbe74687879e5137bb8d274f5e8
Alexandr Mazur <mazur.alexandr228@yandex.ru>
2024-10-29 14:07:16 +0300

    изменения (#1)
    Co-authored-by: Alexandr Mazur <aimazur@edu.hse.ru>

--- connectapp/person/views.py
+++ connectapp/person/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render
-
+from users.models import User
+from .models import Article
 
 def profile(request):
-    return render(request, "person/profile.html")
+    news = User.objects.all()
+    news1 = Article.objects.order_by('-date')
+    return render(request, "person/profile.html", {'news': news, 'news1' : news1})
+
+def create(request):
+    return render(request, 'person/create.html')
