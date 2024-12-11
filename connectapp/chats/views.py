c15003855bca812c45dc4b5c407c9495f61e0b5a
EKProgrammer <egor.s.korchagin@gmail.com>
2024-12-11 16:21:10 +0300

    wrote the proper way for the chat rooms to work

--- connectapp/chats/views.py
+++ connectapp/chats/views.py
@@ -11,16 +11,49 @@ def start_chat(request, user_id):
     return redirect('chats', chat_id=chat.id)
 
 
+def get_user_chats(request):
+    """Возвращает отфильтрованный список чатов для текущего пользователя."""
+    chats_list = Chat.objects.filter(participants=request.user)
+    chats_list_filtered = []
+    for chat in chats_list:
+        other_participants = chat.participants.exclude(id=request.user.id)
+        for user in other_participants:
+            chats_list_filtered.append({'user': user, 'chat_id': chat.id})
+    return chats_list_filtered
+
+
+@login_required
+def chats_empty_page(request):
+    chats_list = get_user_chats(request)
+    data = {
+        'user': request.user,
+        'chats_list': chats_list,
+        'chat': None,
+        'messages': None
+    }
+    return render(request, 'chats/chats.html', data)
+
+
 @login_required
 def chats(request, chat_id):
     chat = get_object_or_404(Chat, id=chat_id)
     messages = chat.messages.all()
-    print(list(chat.participants.all()))
-    
+
     if request.method == 'POST':
         content = request.POST.get('message-content')
         if content:
             Message.objects.create(chat=chat, sender=request.user, content=content)
         return redirect('chats', chat_id=chat.id)
-    
-    return render(request, 'chats/chats.html', {'chat': chat, 'messages': messages})
+
+    if not chat.participants.filter(id=request.user.id).exists():
+        return render(request, "404.html")
+
+    chats_list = get_user_chats(request)
+
+    data = {
+        'user': request.user,
+        'chats_list': chats_list,
+        'chat': chat,
+        'messages': messages
+    }
+    return render(request, 'chats/chats.html', data)
