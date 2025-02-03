927e640b4fe75c5fd7e76bcabdf4bde5e5d6efc6
Alexandr Mazur <mazur.alexandr228@yandex.ru>
2025-02-04 00:16:01 +0300

    Added editing and deleting of messages

--- connectapp/chats/urls.py
+++ connectapp/chats/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     path('start-chat/<int:user_id>', views.start_chat, name='start_chat'),
     path('<int:chat_id>', views.chats, name='chats'),
     path('message_generation', views.mistral_message_generation, name='message_generation'),
+    path('edit_message/', views.edit_message, name='edit_message'),
+    path('delete_message/<int:message_id>/', views.delete_message, name='delete_message'),
 ]
