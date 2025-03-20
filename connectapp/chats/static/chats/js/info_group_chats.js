5aecf5420cd78419edb7bd1fa60e8f65e5e23117
Alexandr Mazur <mazur.alexandr228@yandex.ru>
2025-03-20 17:15:54 +0300

    add delete chats and info chats
    Co-authored-by: Александр Мазур Иванович <aimazur@edu.hse.ru>

---
+++ connectapp/chats/static/chats/js/info_group_chats.js
@@ -0,0 +1,22 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const panelHeader = document.querySelector(".chat-name-window");
+    const chatWindow = document.getElementById("chat-messages"); 
+    const inputArea = document.querySelector(".input-area"); 
+    const chatInfoPanel = document.getElementById("chat-info-panel"); 
+
+    if (panelHeader) {
+        panelHeader.addEventListener("click", function () {
+            const currentDisplay = window.getComputedStyle(chatInfoPanel).display;
+            if (currentDisplay === "none" || currentDisplay === "") {
+                chatInfoPanel.style.display = "block";
+                chatWindow.style.display = "none"; 
+                inputArea.style.display = "none"; 
+                panelHeader.style.display = "none"; 
+            } else {
+                chatInfoPanel.style.display = "none";
+                chatWindow.style.display = "block"; 
+                inputArea.style.display = "block"; 
+            }
+        });
+    }
+});
